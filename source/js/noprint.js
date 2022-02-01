bc12793450e2a45d89b04d8395ff1fb2cff3ba5d
p1slave <p1slave@protonmail.com>
2022-02-01 03:52:13 -0600

    Modify noprint.js to remove changes to css style

--- source/js/noprint.js
+++ source/js/noprint.js
@@ -1,12 +1,13 @@
 /*
 NoPrint.js V1.0
 Created by PDFAntiCopy.com
+Modified by p1slave to disable any change to css style
 */
 if (noCopy) 
 { 
-	document.body.oncopy = function(){return false}; 
-	document.body.oncontextmenu = function(){return false}; 
-	document.body.onselectstart = document.body.ondrag = function(){
+	document.body.oncopy = function(){ return false }; 
+	document.body.oncontextmenu = function(){ return false }; 
+	document.body.onselectstart = document.body.ondrag = function() {
 	    return false;
 	}
  	document.onkeydown = function() {
@@ -16,72 +17,68 @@ if (noCopy)
 	}
 }
 
-if (noPrint) 
-{     
-   var c=document.createElement("span");
-   c.style.display="none";
-   c.style.postion="absolute";
-   c.style.background="#000";
-	var first=document.body.firstChild;
-	var wraphtml=document.body.insertBefore(c,first);
-  c.setAttribute('width', document.body.scrollWidth);
-  c.setAttribute('height', document.body.scrollHeight);
- 	c.style.display="block";
- 	var cssNode3 = document.createElement('style'); 
-	cssNode3.type = 'text/css'; 
-	cssNode3.media = 'print'; 
-	cssNode3.innerHTML ='body{display:none}';
-		document.head.appendChild(cssNode3); 	
-}         	
+// if (noPrint) 
+// {     
+// 	var c=document.createElement("span");
+//     c.style.display="none";
+//     c.style.postion="absolute";
+//     c.style.background="#000";
+// 	var first=document.body.firstChild;
+// 	var wraphtml=document.body.insertBefore(c,first);
+//     c.setAttribute('width', document.body.scrollWidth);
+//     c.setAttribute('height', document.body.scrollHeight);
+//  	c.style.display="block";
+//  	var cssNode3 = document.createElement('style'); 
+// 	cssNode3.type = 'text/css'; 
+// 	cssNode3.media = 'print'; 
+// 	cssNode3.innerHTML ='body{display:none}';
+// 	document.head.appendChild(cssNode3); 	
+// }         	
 	
-var cssNode2 = document.createElement('style'); 
-cssNode2.type = 'text/css'; 
-cssNode2.media = 'screen'; 
-cssNode2.innerHTML ='div{-webkit-touch-callout: none;-webkit-user-select: none;-khtml-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;}';
-document.head.appendChild(cssNode2);
-document.body.style.cssText="-webkit-touch-callout: none;-webkit-user-select: none;-khtml-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;";
+// var cssNode2 = document.createElement('style'); 
+// cssNode2.type = 'text/css'; 
+// cssNode2.media = 'screen'; 
+// cssNode2.innerHTML ='div{-webkit-touch-callout: none;-webkit-user-select: none;-khtml-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;}';
+// document.head.appendChild(cssNode2);
+// document.body.style.cssText="-webkit-touch-callout: none;-webkit-user-select: none;-khtml-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;";
  	
 
-function toBlur()
-{
-	if (autoBlur)
-	document.body.style.cssText="-webkit-filter: blur(5px);-moz-filter: blur(5px);-ms-filter: blur(5px);-o-filter: blur(5px);filter: blur(5px);"
-}
+// function toBlur() {
+// 	if (autoBlur) {
+// 		document.body.style.cssText="-webkit-filter: blur(5px);-moz-filter: blur(5px);-ms-filter: blur(5px);-o-filter: blur(5px);filter: blur(5px);"
+// 	}
+// }
 
-function toClear()
-{
-	document.body.style.cssText="-webkit-filter: blur(0px);-moz-filter: blur(0px);-ms-filter: blur(0px);-o-filter: blur(0px);filter: blur(0px);"
-}
+// function toClear() {
+// 	document.body.style.cssText="-webkit-filter: blur(0px);-moz-filter: blur(0px);-ms-filter: blur(0px);-o-filter: blur(0px);filter: blur(0px);"
+// }
 
-document.onclick = function(event){
- 	toClear();
-}
+// document.onclick = function(event){
+//  	toClear();
+// }
  
-document.onmouseleave = function(event){
-	toBlur();
-}
+// document.onmouseleave = function(event){
+// 	toBlur();
+// }
 
-document.onblur = function(event){
- 	toBlur();
-}
+// document.onblur = function(event) {
+//  	toBlur();
+// }
 
 document.addEventListener('keyup', (e) => {
     if (e.key == 'PrintScreen') {
-    	if (noScreenshot)
-    	{
-        navigator.clipboard.writeText('');
-        
-      }
+    	if (noScreenshot) {
+        	navigator.clipboard.writeText('');
+        }
     }
 });
 
 document.addEventListener('keydown', (e) => {
     if (e.ctrlKey && e.key == 'p') {
-        if (noPrint)
-	    	{
+        if (noPrint) {
 	        e.cancelBubble = true;
 	        e.preventDefault();
 	        e.stopImmediatePropagation();
-	      }
+	    }
     }
 });
