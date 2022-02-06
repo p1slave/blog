63bc8143e978e86481e06eac4b8922b507ae1494
p1slave <p1slave@protonmail.com>
2022-02-06 01:04:30 -0600

    Add BlogAssetManager to handle assets encryption and watermarking

--- utils/watermark/run.py
+++ utils/watermark/run.py
@@ -17,7 +17,7 @@ def convert2png(image_path, output_path):
 
 # Convert to RGBA mode first to include the transparency and then invert 
 # RGB channel but leave the transparency unchanged
-def invert_transparent_img(image_path, output_path):
+def invert_transparent_img(image_path):
     img = Image.open(image_path)
     if img.mode != 'RGBA':
         img = img.convert('RGBA')
@@ -30,8 +30,26 @@ def invert_transparent_img(image_path, output_path):
     r, g, b = map(invert, (r, g, b))
 
     img2 = Image.merge(img.mode, (r, g, b, a))
+    return img2
 
-    img2.save(output_path, "PNG")
+
+def make_grayish_img(image_path):
+    # Less than (125, 125, 125) to (25, 25, 25)
+    # Greater than (125, 125, 125) to (225, 225, 225)
+    img = Image.open(image_path)
+    if img.mode != 'RGBA':
+        img = img.convert('RGBA')
+
+    r, g, b, a = img.split()
+
+    def invert(image):
+        return image.point(lambda p: 25 if p < 125 else 225)
+
+    r, g, b = map(invert, (r, g, b))
+
+    img2 = Image.merge(img.mode, (r, g, b, a))
+    return img2
+
 
 # The blind watermark is not working well on screenshots and may need more tuning on params
 def add_blind_watermark(original_file, watermark_file, output_file):
@@ -82,8 +100,9 @@ def reduce_opacity(image, opacity):
 
     return image
 
-def add_string_watermark(image, wm_str, opacity=0.8):
+def add_string_watermark(image, wm_str, opacity=0.5):
     assert opacity >= 0 and opacity <= 1
+    # Set the opacity lower than 0.5 ideally to avoid covering the original photo
     if image.mode != 'RGBA':
         image = image.convert('RGBA')
 
@@ -91,10 +110,11 @@ def add_string_watermark(image, wm_str, opacity=0.8):
     h = image.size[1]
     font_size = h // 15
 
-    txt = Image.new('RGBA', image.size, (0,0,0,0))
+    txt = Image.new('RGBA', image.size, (0, 0, 0, 0))
     draw = ImageDraw.Draw(txt)
     font = ImageFont.truetype("party-confetti.ttf", font_size)
-    draw.text((0, 0), wm_str, (255, 255, 255, 255), font=font)
+    # Do not use white text (255, 255, 255) because it won't be visible on white background
+    draw.text((0, 0), wm_str, (225, 225, 225, 255), font=font)
     
     txt = reduce_opacity(txt, opacity)
     
@@ -105,7 +125,7 @@ def add_string_watermark(image, wm_str, opacity=0.8):
     watermark_image = Image.composite(txt, image, txt)
     return watermark_image
 
-def add_watermark(image, mark, opacity=0.8):
+def add_watermark(image, mark, opacity=0.5):
     if opacity < 1:
         mark = reduce_opacity(mark, opacity)
 
@@ -213,47 +233,128 @@ def gpg_decrypt_file(encrypted_file_path, passphrase, output_dir=None):
 # wm_image = add_string_watermark(image, "p1slave.com", opacity=0.5)
 # add_double_watermarks(image, mark, "p1slave.com", opacity=0.8).save("watermarked.png", "PNG")
 
+
+class BlogAssetManager():
+    def __init__(self, posts_dir, keyid="p1slave", logo_file="logo.png", signature="p1slave.com"):
+        self.posts_dir = posts_dir
+        self.keyid = keyid
+        self.logo_file = logo_file
+        self.signature = signature
+
+        if not os.path.isfile(self.logo_file):
+            print("Provide a valid logo file")
+
+    def add_file(self, post_name, file_path):
+        if not os.path.isfile(file_path):
+            print("File does not exist: " + file_path)
+            return
+
+        _, filename = os.path.split(file_path) 
+        selected_post_dir = os.path.join(self.posts_dir, post_name)
+
+        encryption_dir = os.path.join(selected_post_dir, "encrypted")
+        encrypted_file = os.path.join(encryption_dir, filename + ".asc")
+
+        decryption_dir = os.path.join(selected_post_dir, "decrypted")
+        decrypted_file = os.path.join(decryption_dir, filename)
+
+        if os.path.isfile(decrypted_file):
+            overwritten = input("Decrypted file %s already exists. Overwrite? (y/n)" % decrypted_file)
+            if not overwritten.startswith("y"):
+                print("Abort")
+                return
+
+        # Copy the new file to the decryption folder
+        shutil.copyfile(file_path, decrypted_file)
+        print("Copied %s to %s" % (file_path, decrypted_file))
+
+        if os.path.isfile(encrypted_file): 
+            overwritten = input("Encrypted file %s already exists. Overwrite? (y/n)" % encrypted_file)
+            if not overwritten.startswith("y"):
+                print("Abort")
+                return
+
+        # Encrypt the file and add it to the encryption folder
+        status = gpg_encrypt_file(file_path, keyid, output_dir=encryption_dir)
+        print("Encrypting file as %s: %s" % (encrypted_file, status))
+
+    def remove_encryption_folders(self):
+        for root_dir, _, _ in os.walk(self.posts_dir):
+            # Remove all folders containing encrypted files
+            if root_dir.endswith("encrypted"):
+                shutil.rmtree(root_dir)
+
+    def remove_decryption_folders(self):
+        for root_dir, _, _ in os.walk(self.posts_dir):
+            # Remove all folders containing decrypted files
+            if root_dir.endswith("decrypted"):
+                shutil.rmtree(root_dir)
+
+    def remove_watermarked_assets(self):
+        for root_dir, dirs, files in os.walk(self.posts_dir):
+            # Find the asset folder for each post
+            if root_dir != self.posts_dir and os.path.dirname(root_dir) == self.posts_dir:
+                for file in files:
+                    os.remove(os.path.join(root_dir, file))
+
+    def encrypt_post_assets(self):
+        for root_dir, dirs, files in os.walk(self.posts_dir):
+            # Only encrypt files in the the decrypted folder and add new files to this folder
+            if root_dir != self.posts_dir and root_dir.endswith("decrypted"):
+                print("The direcotry %s has folders: %s and files: %s" % (root_dir, dirs, files))
+                encryption_dir = os.path.abspath(os.path.join(root_dir, os.pardir, "encrypted"))
+                for file in files:
+                    file_path = os.path.join(root_dir, file)
+                    # The existing encrypted file will be overwritten
+                    status = gpg_encrypt_file(file_path, keyid, output_dir=encryption_dir)
+                    print("Encrypting %s: %s" % (file_path, status))
+
+    def decrypt_post_assets(self):
+        passphrase = input("Enter passphrase for GPG private key: ")
+        for root_dir, _, files in os.walk(self.posts_dir):
+            if root_dir.endswith("encrypted"):
+                print("Found encryption folder: %s" % root_dir)
+                # Create a decryption folder in the parent directory of encryption folder
+                decryption_dir = os.path.abspath(os.path.join(root_dir, os.pardir, "decrypted"))
+                for encrypted_file in files:
+                    encrypted_file_path = os.path.join(root_dir, encrypted_file)
+                    # The existing decrypted file will be overwritten
+                    status = gpg_decrypt_file(encrypted_file_path, passphrase, output_dir=decryption_dir)
+                    print("Decrypting %s: %s" % (encrypted_file_path, status))
+                
+    def watermark_post_assets(self):
+        for root_dir, dirs, files in os.walk(self.posts_dir):
+            # Only encrypt files in the the decrypted folder and add new files to this folder
+            if root_dir != self.posts_dir and root_dir.endswith("decrypted"):
+                print("The direcotry %s has folders: %s and files: %s" % (root_dir, dirs, files))
+                # Asset folder is the parent directory of decrypted folder to hold watermarked files
+                asset_dir = os.path.abspath(os.path.join(root_dir, os.pardir))
+                for file in files:
+                    decrypted_file_path = os.path.join(root_dir, file)
+                    pre, _ = os.path.splitext(file)
+                    # Convert to JPEG format to reduce the size
+                    asset_file_path = os.path.join(asset_dir, pre + ".jpg")
+                    if file.split('.')[-1] in ["jpg", "jpeg", "png"]:
+                        png_image = add_double_watermarks(
+                            Image.open(decrypted_file_path), 
+                            # Image.open(self.logo_file), 
+                            make_grayish_img(self.logo_file),
+                            self.signature, 
+                            opacity=0.6
+                        )
+                        png_image.convert("RGB").save(asset_file_path)
+
+
 current_dir = os.getcwd()
 repo_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir, os.pardir))
-logo_file = os.path.join(repo_dir, "source/images/p1slave-logo.png")
-mark = Image.open(logo_file)
+posts_dir = os.path.join(repo_dir, "source/_posts")
 
 keyid = "p1slave"
-passphrase = input("Enter passphrase for GPG private key: ")
-post_dir = os.path.join(repo_dir, "source/_posts")
+logo_file = os.path.join(repo_dir, "source/images/p1slave-logo.png")
 
-def remove_encryption_folders():
-    for root_dir, _, _ in os.walk(post_dir):
-        # Remove all folders containing encrypted files
-        if root_dir.endswith("encrypted"):
-            shutil.rmtree(root_dir)
+manager = BlogAssetManager(posts_dir, keyid=keyid, logo_file=logo_file, signature="p1slave.com")
 
-def encrypt_post_assets():
-    for root_dir, dirs, files in os.walk(post_dir):
-        # Remove all folders containing encrypted files
-        # if root_dir.endswith("encrypted"):
-        #     shutil.rmtree(root_dir)
-
-        if root_dir != post_dir and not root_dir.endswith("encrypted"):
-            print("The direcotry %s has folders: %s and files: %s" % (root_dir, dirs, files))
-            output_dir = os.path.join(root_dir, "encrypted")
-            for file in files:
-                file_path = os.path.join(root_dir, file)
-                status = gpg_encrypt_file(file_path, keyid, output_dir=output_dir)
-                print("Encrypting %s: %s" % (file_path, status))
-
-def decrypt_post_assets():
-    for root_dir, _, files in os.walk(post_dir):
-        if root_dir.endswith("encrypted"):
-            print("Found encryption folder: %s" % root_dir)
-            decryption_dir = os.path.abspath(os.path.join(root_dir, os.pardir, "decrypted"))
-            for encrypted_file in files:
-                encrypted_file_path = os.path.join(root_dir, encrypted_file)
-                status = gpg_decrypt_file(encrypted_file_path, passphrase, output_dir=decryption_dir)
-                print("Decrypting %s: %s" % (encrypted_file_path, status))
-            
-def watermark_post_assets():
-    pass
-
-
-# decrypt_post_assets()
+# manager.decrypt_post_assets()
+# manager.encrypt_post_assets()
+manager.watermark_post_assets()
+# manager.remove_watermarked_assets()
